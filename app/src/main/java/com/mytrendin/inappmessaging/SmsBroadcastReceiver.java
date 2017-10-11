5b0b48c2a1be844e845e590ada1c3f3303daf876
Kartik Arora <chipset95@yahoo.co.in>
2017-10-11 13:26:34 +0530

    UI overhaul, issue #2

--- app/src/main/java/com/mytrendin/inappmessaging/SmsBroadcastReceiver.java
+++ app/src/main/java/com/mytrendin/inappmessaging/SmsBroadcastReceiver.java
@@ -1,16 +1,12 @@
 package com.mytrendin.inappmessaging;
 
-import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.media.AudioManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.telephony.SmsMessage;
 import android.util.Log;
 import android.widget.Toast;
@@ -18,15 +14,16 @@ import android.widget.Toast;
 public class SmsBroadcastReceiver extends BroadcastReceiver {
     public AudioManager audioManager;
     SharedPreferences sharedPreferences;
-    boolean b=false;
+    boolean b = false;
     String smsBody;
     final int REQUEST_CODE_ASK_PERMISSIONS = 123;
 
     public static final String SMS_BUNDLE = "pdus";
 
     SmsMessage smsMessage;
+
     public void onReceive(Context context, Intent intent) {
-        Toast.makeText(context.getApplicationContext(),"Toast Long",Toast.LENGTH_LONG).show();
+        Toast.makeText(context.getApplicationContext(), "Toast Long", Toast.LENGTH_LONG).show();
 
 
         sharedPreferences = context.getApplicationContext().getSharedPreferences("Pritom", context.MODE_PRIVATE);
@@ -56,8 +53,6 @@ public class SmsBroadcastReceiver extends BroadcastReceiver {
             }
             Toast.makeText(context, "SMS : " + smsBody + pass3, Toast.LENGTH_SHORT).show();
             Log.d("Message:", smsBody + pass3);
-
-
 
 
         }
